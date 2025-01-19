3657b86b4d4b63d6e2521b365d135b1c5e4bdef9
Prajapatirajat94 <prajapatirajat94@gmail.com>
2025-01-18 23:23:06 -0500

    longpress

--- src/main/java/MobileTestinLaunch/IOsMobileLaunch.java
+++ src/main/java/MobileTestinLaunch/IOsMobileLaunch.java
@@ -1,5 +1,31 @@
 package MobileTestinLaunch;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
+
 public class IOsMobileLaunch {
+	public static IOSDriver driver;
+
+	public static void main(String[] args) {
+
+		UiAutomator2Options option = new UiAutomator2Options();
+		option.setDeviceName("");
+		option.setApp("");
+
+		try {
+			driver = new IOSDriver(new URL(""), option);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		driver.findElement(AppiumBy.iOSNsPredicateString(""));
+		driver.findElement(AppiumBy.iOSClassChain(""));
+
+	}
 
 }
